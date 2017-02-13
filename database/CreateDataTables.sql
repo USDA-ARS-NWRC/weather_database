aad0c3bbc031b76da80197c49970de65d316303d
Micah Johnson <micah.johnson150@gmail.com>
2017-02-13 15:36:35 -0700

    Took out the ignore statements as it was deprecated while ago and now throws an error with newer mysql

--- database/CreateDataTables.sql
+++ database/CreateDataTables.sql
@@ -22,14 +22,20 @@ CREATE TABLE IF NOT EXISTS tbl_raw_data (
 
 -- tbl_level1 is the automatic data QC and organization
 CREATE TABLE IF NOT EXISTS tbl_level1 LIKE tbl_raw_data;
-ALTER IGNORE TABLE tbl_level1 ADD COLUMN av_del TINYINT(1) DEFAULT 0;
+ALTER TABLE tbl_level1 ADD COLUMN av_del TINYINT(1) DEFAULT 0;
+
+
+-- tbl_level1 is the automatic data QC and organization
+CREATE TABLE IF NOT EXISTS tbl_level1_5 LIKE tbl_raw_data;
+ALTER TABLE tbl_level1_5 ADD COLUMN clipped TINYINT(1) DEFAULT 0;
+
 
 -- tbl_level2 is the users accessing the db and changing the data
 CREATE TABLE IF NOT EXISTS tbl_level2 LIKE tbl_raw_data;
 
-ALTER IGNORE TABLE tbl_level2 ADD COLUMN user VARCHAR(20) FIRST,
+ALTER TABLE tbl_level2 ADD COLUMN user VARCHAR(20) FIRST,
 ADD COLUMN date_fixed DATETIME AFTER user;
 
-ALTER IGNORE TABLE tbl_level2 ADD COLUMN precip_intensity DECIMAL(10,5) AFTER precip_accum;
+ALTER TABLE tbl_level2 ADD COLUMN precip_intensity DECIMAL(10,5) AFTER precip_accum;
 
 -- ALTER IGNORE TABLE tbl_level1 ADD COLUMN cloud_factor DECIMAL(10 , 3);
