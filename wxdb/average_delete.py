89fbfb39442ca16b625e09e1079fef9a127a8149
Scott Havens <scott.havens@ars.usda.gov>
2017-08-11 12:29:03 -0600

    average_delete running, a big difference is that it will round to the closest hour

--- wxdb/average_delete.py
+++ wxdb/average_delete.py
@@ -63,17 +63,21 @@ class AvgDel():
         determine the whole hours and how many values are present
         """
         
-        qry = "SELECT COUNT(*) AS cnt, station_id, FROM_UNIXTIME(FLOOR((UNIX_TIMESTAMP(date_time) + %i-1) / %i) * %i) AS round_date \
-                    FROM tbl_level1 WHERE station_id='%s' and avg_del=0 GROUP BY station_id,round_date" % (round_val, round_val, round_val, sta)
+        qry = ("SELECT COUNT(*) AS cnt, MIN(id), station_id, "
+               "DATE_ADD(DATE_FORMAT(date_time, \'%Y-%m-%d %H:00:00\'), "
+               "INTERVAL IF(MINUTE(date_time) < {}/60/2, 0, 1) HOUR) AS round_date "
+               "FROM tbl_level1 WHERE station_id='{}' and avg_del=0 GROUP BY "
+               "station_id,round_date").format(round_val, sta)
         self.cursor.execute(qry)
         d = self.cursor.fetchall()
         
         # create a dataframe for easy viewing/manipulating
-        data = pd.DataFrame(d, columns=['cnt', 'station_id', 'round_time'])
+        data = pd.DataFrame(d, columns=['cnt', 'id', 'station_id', 'round_time'])
+        data['round_time']  = pd.to_datetime(data['round_time'])
         
         # make sure that all the round_values are unique
         if len(data) != len(data.round_time.unique()):
-            raise Exception('Times are not unique for station %s' % sta)
+            raise Exception('Times are not unique for station {}'.format(sta))
         
         flag = False
         if len(data) == data.cnt.sum():
@@ -88,19 +92,20 @@ class AvgDel():
         """
         
         # create a query to get all the values
-        qry = "SELECT * FROM %s WHERE station_id='%s' AND date_time BETWEEN '%s' AND '%s'" % (table, sta, date_start, date_end)
+        qry = "SELECT * FROM {} WHERE station_id='{}' AND date_time BETWEEN '{}' AND '{}'" .format(table, sta, date_start, date_end)
         dup = pd.read_sql(qry, self.db.cnx, index_col='date_time')
         
         return dup
     
     
-    def delete_values(self, sta, date_start, date_end, table):
+    def delete_values(self, ids, table='tbl_level1'):
         """
         Delete values from a table between a time range
         """
         
-        delete_qry = "DELETE FROM %s WHERE station_id='%s' AND date_time BETWEEN '%s' AND '%s'" % (table, sta, date_start, date_end)
-        return self.cursor.execute(delete_qry)
+        wildcards = ','.join(['%s'] * len(ids))
+        delete_qry = "DELETE FROM {} WHERE id IN ({})".format(table, wildcards)
+        return self.cursor.execute(delete_qry, ids)
     
     
     def average_reinsert(self, sta, data, date_time, table='tbl_level1'):
@@ -110,48 +115,30 @@ class AvgDel():
         
         m = data.mean()
         m['station_id'] = sta
-        m['date_time'] = None
-        m['date_time'] = pd.Timestamp(date_time)
-        m['av_del'] = 1
+        m['date_time'] = pd.Timestamp(date_time).strftime('%Y-%m-%d %H:%M')
+        m['avg_del'] = 1
+        m = m.where((pd.notnull(m)), None)
         
         # create a query to insert the average values
         cols = m.keys().tolist()
-        val = []
-        for c in cols:
-            val.append(str(m[c]))
         
-        insert_qry = "INSERT INTO %s (%s) VALUES ('%s')" % (table, ",".join(cols), "','".join(val))
-        self.cursor.execute(insert_qry)
+        wildcards = ','.join(['%s'] * len(cols))
+        colnames = ','.join(cols)
+        insert_qry = 'INSERT INTO {0} ({1}) VALUES ({2})'.format(
+            table, colnames, wildcards)
+        
+        in_data = tuple(m.values)
+                
+        self.cursor.execute(insert_qry, in_data)
         
         
-    def single_timestep(self, sta, date_time, round_time, flag, round_val):
+    def single_timestep(self, sta, id, round_time, flag, round_val):
         """
         Create a query for a given time
         """
-        
-        # only one time step for the rounding
-        # check to see if the date_time is equal to round_time
-        if date_time != round_time:
-            
-            if flag:
-                # then all values are on the hour but need to be adjusted
-                # so this takes a little different approach and rounds to the nearest
-                # time step value
-                t = timedelta(minutes=date_time.minute, seconds=date_time.second)
-                
-                dt = date_time - t
-                if t >= timedelta(minutes=round_val/60/2):
-                    dt += timedelta(minutes=round_val/60)
-                
-                qry = "UPDATE tbl_level1 SET date_time='%s', avg_del=1 where date_time='%s' and station_id='%s'" % (dt, date_time, sta)
-                
-            else:  
-                qry = "UPDATE tbl_level1 SET date_time='%s', avg_del=1 where date_time='%s' and station_id='%s'" % (round_time, date_time, sta)
-                        
-        else:
-            # change the avg_del column to ignore next time
-            qry = "UPDATE tbl_level1 SET avg_del=1 where date_time='%s' and station_id='%s'" % (date_time, sta)
-                        
+ 
+        qry = "UPDATE tbl_level1 SET date_time='{}', avg_del=1 where id='{}'".format(round_time, id)
+                     
         return qry            
     
     
@@ -163,7 +150,7 @@ class AvgDel():
         """
          
         try:
-            self.db.cnx.start_transaction() # start a transaction
+#             self.db.cnx.start_transaction() # start a transaction
              
              
             # check the data from tbl_raw_data
@@ -194,19 +181,22 @@ class AvgDel():
             # determine the whole hours and how many values are present
              
             data, flag = self.get_station_data(sta, self.round_val)
+            
+            self._logger.debug('{} - {} times to process'.format(sta, len(data)))
              
             # loop through each row
             for i,r in data.iterrows():
                  
                 # determine the time step start point
-                date_start = r.round_time - timedelta(seconds=self.round_val-1)
+                date_start = r.round_time - timedelta(seconds=self.round_val/2-1)
+                date_end = r.round_time + timedelta(seconds=self.round_val/2)
                  
                 if r.cnt == 1:
                     try:
-                        self.db.cnx.start_transaction() # start a transaction
+#                         self.db.cnx.start_transaction() # start a transaction
                          
                         # create query for single time step
-                        qry = self.single_timestep(sta, r.date_time, r.round_time, flag, self.round_val)
+                        qry = self.single_timestep(sta, r.id, r.round_time, flag, self.round_val)
                          
                         self.cursor.execute(qry)
                         self.db.cnx.commit()
@@ -227,13 +217,13 @@ class AvgDel():
                 else:
                     try:
                     # there are multiple values for each time step
-                        self.db.cnx.start_transaction() # start a transaction
+#                         self.db.cnx.start_transaction() # start a transaction
                          
                         # get all the values from the table
-                        dup = self.get_values(sta, date_start, r.round_time, 'tbl_level1')
+                        dup = self.get_values(sta, date_start, date_end, 'tbl_level1')
                          
                         # delete all those values from the table
-                        result = self.delete_values(sta, date_start, r.round_time, 'tbl_level1')
+                        result = self.delete_values(dup['id'].values.tolist(), 'tbl_level1')
                          
                         # get the average and reinsert into table
                         self.average_reinsert(sta, dup, r.round_time, 'tbl_level1')
