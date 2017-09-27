4034effdb5dc2116376666cd2c13627f1f6e7234
Scott Havens <scott.havens@ars.usda.gov>
2017-09-27 16:32:00 -0600

    triggers, updated user command and cleaned up the trigger create

--- database_setup/schema/tbl_level2_AFTER_INSERT.sql
+++ database_setup/schema/tbl_level2_AFTER_INSERT.sql
@@ -1,67 +1,71 @@
 DELIMITER $$
 
-CREATE DEFINER = CURRENT_USER TRIGGER `weather_db`.`tbl_level2_AFTER_INSERT` AFTER INSERT ON `tbl_level2` FOR EACH ROW
+CREATE TRIGGER `weather_db`.`tbl_level2_AFTER_INSERT` AFTER INSERT ON `tbl_level2` FOR EACH ROW
 BEGIN
 
-if new.air_temp is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'air_temp',new.air_temp);
-end if;
+	-- Find username of person performing the DELETE into table
+    DECLARE vUser varchar(50);
+	SELECT USER() INTO vUser;
 
-if new.dew_point_temperature is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'dew_point_temperature',new.dew_point_temperature);
-end if;
+	if new.air_temp is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'air_temp',new.air_temp);
+	end if;
 
-if new.relative_humidity is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'relative_humidity',new.relative_humidity);
-end if;
+	if new.dew_point_temperature is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'dew_point_temperature',new.dew_point_temperature);
+	end if;
 
-if new.wind_speed is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'wind_speed',new.wind_speed);
-end if;
+	if new.relative_humidity is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'relative_humidity',new.relative_humidity);
+	end if;
 
-if new.wind_direction is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'wind_direction',new.wind_direction);
-end if;
+	if new.wind_speed is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'wind_speed',new.wind_speed);
+	end if;
 
-if new.wind_gust is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'wind_gust',new.wind_gust);
-end if;
+	if new.wind_direction is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'wind_direction',new.wind_direction);
+	end if;
 
-if new.solar_radiation is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'solar_radiation',new.solar_radiation);
-end if;
+	if new.wind_gust is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'wind_gust',new.wind_gust);
+	end if;
 
-if new.snow_smoothed is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_smoothed',new.snow_smoothed);
-end if;
+	if new.solar_radiation is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'solar_radiation',new.solar_radiation);
+	end if;
 
-if new.precip_accum is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'precip_accum',new.precip_accum);
-end if;
+	if new.snow_smoothed is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_smoothed',new.snow_smoothed);
+	end if;
 
-if new.precip_intensity is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'precip_intensity',new.precip_intensity);
-end if;
+	if new.precip_accum is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'precip_accum',new.precip_accum);
+	end if;
 
-if new.snow_depth is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_depth',new.snow_depth);
-end if;
+	if new.precip_intensity is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'precip_intensity',new.precip_intensity);
+	end if;
 
-if new.snow_interval is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_interval',new.snow_interval);
-end if;
+	if new.snow_depth is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_depth',new.snow_depth);
+	end if;
 
-if new.snow_water_equiv is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_water_equiv',new.snow_water_equiv);
-end if;
+	if new.snow_interval is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_interval',new.snow_interval);
+	end if;
 
-if new.vapor_pressure is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'vapor_pressure',new.vapor_pressure);
-end if;
+	if new.snow_water_equiv is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'snow_water_equiv',new.snow_water_equiv);
+	end if;
 
-if new.cloud_factor is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'cloud_factor',new.cloud_factor);
-end if;
+	if new.vapor_pressure is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'vapor_pressure',new.vapor_pressure);
+	end if;
+
+	if new.cloud_factor is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'insert',CURRENT_USER(),CURRENT_TIMESTAMP(),new.id,'cloud_factor',new.cloud_factor);
+	end if;
 
 END;$$
 
--- database_setup/schema/tbl_level2_BEFORE_DELETE.sql
+++ database_setup/schema/tbl_level2_BEFORE_DELETE.sql
@@ -1,66 +1,71 @@
 DELIMITER $$
 
-CREATE DEFINER = CURRENT_USER TRIGGER `weather_db`.`tbl_level2_BEFORE_DELETE` BEFORE DELETE ON `tbl_level2` FOR EACH ROW
+CREATE TRIGGER `weather_db`.`tbl_level2_BEFORE_DELETE` BEFORE DELETE ON `tbl_level2` FOR EACH ROW
 BEGIN
-if old.air_temp is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'air_temp',old.air_temp);
-end if;
 
-if old.dew_point_temperature is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'dew_point_temperature',old.dew_point_temperature);
-end if;
+	-- Find username of person performing the DELETE into table
+    DECLARE vUser varchar(50);
+	SELECT USER() INTO vUser;
 
-if old.relative_humidity is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'relative_humidity',old.relative_humidity);
-end if;
+	if old.air_temp is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'air_temp',old.air_temp);
+	end if;
 
-if old.wind_speed is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'wind_speed',old.wind_speed);
-end if;
+	if old.dew_point_temperature is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'dew_point_temperature',old.dew_point_temperature);
+	end if;
 
-if old.wind_direction is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'wind_direction',old.wind_direction);
-end if;
+	if old.relative_humidity is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'relative_humidity',old.relative_humidity);
+	end if;
 
-if old.wind_gust is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'wind_gust',old.wind_gust);
-end if;
+	if old.wind_speed is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'wind_speed',old.wind_speed);
+	end if;
 
-if old.solar_radiation is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'solar_radiation',old.solar_radiation);
-end if;
+	if old.wind_direction is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'wind_direction',old.wind_direction);
+	end if;
 
-if old.snow_smoothed is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_smoothed',old.snow_smoothed);
-end if;
+	if old.wind_gust is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'wind_gust',old.wind_gust);
+	end if;
 
-if old.precip_accum is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'precip_accum',old.precip_accum);
-end if;
+	if old.solar_radiation is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'solar_radiation',old.solar_radiation);
+	end if;
 
-if old.precip_intensity is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'precip_intensity',old.precip_intensity);
-end if;
+	if old.snow_smoothed is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_smoothed',old.snow_smoothed);
+	end if;
 
-if old.snow_depth is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_depth',old.snow_depth);
-end if;
+	if old.precip_accum is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'precip_accum',old.precip_accum);
+	end if;
 
-if old.snow_interval is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_interval',old.snow_interval);
-end if;
+	if old.precip_intensity is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'precip_intensity',old.precip_intensity);
+	end if;
 
-if old.snow_water_equiv is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_water_equiv',old.snow_water_equiv);
-end if;
+	if old.snow_depth is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_depth',old.snow_depth);
+	end if;
 
-if old.vapor_pressure is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'vapor_pressure',old.vapor_pressure);
-end if;
+	if old.snow_interval is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_interval',old.snow_interval);
+	end if;
 
-if old.cloud_factor is not null then
-	INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'cloud_factor',old.cloud_factor);
-end if;
+	if old.snow_water_equiv is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'snow_water_equiv',old.snow_water_equiv);
+	end if;
+
+	if old.vapor_pressure is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'vapor_pressure',old.vapor_pressure);
+	end if;
+
+	if old.cloud_factor is not null then
+		INSERT INTO `tbl_level2_audit` VALUES (null,'delete',CURRENT_USER(),CURRENT_TIMESTAMP(),old.id,'cloud_factor',old.cloud_factor);
+	end if;
 
 END;$$
 
